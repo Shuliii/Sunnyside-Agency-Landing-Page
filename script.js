4e4916bd2a2d3f6834c589f2ebe2261cd19aa07e
Rico <v_fharsono@tencent.com>
2022-10-21 18:05:48 +0700

    add js and fix some sizes

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const navRightSide = document.querySelector('.nav-right-side');
 const hamburger = document.querySelector('.hamburger');
+const headerContent = document.querySelector('.header-content');
 
-
+hamburger.addEventListener('click', () => {
+    headerContent.classList.toggle('hidden');
+    navRightSide.classList.toggle('visible')
+})
 
